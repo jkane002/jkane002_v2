c544fcc6aef01a53a6fc755998873a9dde287952
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-12-11 22:28:17 -0800

    Set up Portfolio post and Tag models / migrated them

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import PortfolioPost, Tag
 # Register your models here.
+
+admin.site.register(PortfolioPost)
+admin.site.register(Tag)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+class PortfolioPost(models.Model):
+    '''Fields for a single portfolio post'''
+    # Facade
+    title = models.CharField(max_length=200)
+    sub_heading = models.CharField(max_length=200, null=True, blank=True) # can have none
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="/images/placeholder.png")
+    # Details (DetailView?)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    # featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+    # slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+# class BlogPost():
