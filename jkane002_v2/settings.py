f9633f7d330f9744ee6604f929e21940d25e4b5e
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2021-01-30 21:27:12 -0800

    Removed www

--- jkane002_v2/settings.py
+++ jkane002_v2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('JKANE002_V2_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'jkaneshiro.herokuapp.com', 'jkaneshiro.me' , 'www.jkaneshiro.me']
+ALLOWED_HOSTS = ['127.0.0.1', 'jkaneshiro.herokuapp.com', 'jkaneshiro.me']
 
 
 # Application definition
